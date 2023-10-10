b40cbabc1507961206ad8f36b2603efddc16cfe1
jekajolob <jekajolob@gmai.com>
2023-10-10 15:22:47 -0700

    Added productArray(arraySeries#5) solution

--- js/7kyu/productArray(arraySeries#5).js
+++ js/7kyu/productArray(arraySeries#5).js
@@ -1,22 +1,38 @@
 // https://www.codewars.com/kata/5a905c2157c562994900009d/train/javascript
 
-function productArray(numbers){
-    let newArray = []
-    let temp = 1
-    for(i = 0; i <= numbers.length -1; i++){
-        temp = 1
-        for(j = 0; j <= numbers.length -1; j++)
-        if(numbers[i] != numbers[j]){
-            temp *= numbers[j];            
-        }
-        newArray.push(temp)
-        
+//My solution > Wrong
+/* function productArray(numbers){
+  const multiplyArray = [];
 
+  for (let i = 0; i < numbers.length; i++) {
+    let multiply = 1;
+
+    for (let j = 0; j < numbers.length; j++) {
+      if (i === j) {
+        continue;
+      }
+      multiply *= numbers[j];
     }
-    return newArray
+
+    multiplyArray.push(multiply);
   }
 
+  return multiplyArray;
+}
+
+  console.log(productArray([4,4])) // [4,4]
   console.log(productArray([12,20])) // [20,12]
   console.log(productArray([3,27,4,2])) // [216,24,162,324]
-  console.log(productArray([14, 92, 5, 48, 68, 6, 5])) // 
-  //[45043200, 6854400, 126120960, 13137600, 9273600, 105100800, 126120960]
+  console.log(productArray([7, 58, 4, 56, 4, 29, 8, 9])) // 
+  //[108509184, 13095936, 189891072, 13563648, 189891072, 26191872, 94945536, 84396032] */
+
+
+  //Other solution
+  const productArray = numbers =>
+  numbers.map(element => numbers.reduce((prev, curr) => prev * curr) / element)
+
+  console.log(productArray([4,4])) // [4,4]
+  console.log(productArray([12,20])) // [20,12]
+  console.log(productArray([3,27,4,2])) // [216,24,162,324]
+  console.log(productArray([7, 58, 4, 56, 4, 29, 8, 9])) // 
+  //[108509184, 13095936, 189891072, 13563648, 189891072, 26191872, 94945536, 84396032]
