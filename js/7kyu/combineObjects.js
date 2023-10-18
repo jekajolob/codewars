118375e7bf02a5f1ef3d000bc7f30550e12907a5
jekajolob <jekajolob@gmai.com>
2023-10-17 18:05:56 -0700

    Added combine Objects

---
+++ js/7kyu/combineObjects.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/56bd9e4b0d0b64eaf5000819/train/javascript
+
+function combine(...objs) {
+    return objs.reduce((a, b) => {
+        for (let k in b) {
+          if (b.hasOwnProperty(k))
+            a[k] = (a[k] || 0) + b[k];
+        }
+        return a;
+      }, {});
+  }
+
+
+const objA = { a: 10, b: 20, c: 30 }
+const objB = { a: 3, c: 6, d: 3 }
+console.log(combine(objA, objB)) //{ a: 13, b: 20, c: 36, d: 3 }
