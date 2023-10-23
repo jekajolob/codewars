3c6296182d94aadae8ed77ea916a0af3166a4215
jekajolob <jekajolob@gmai.com>
2023-10-23 16:38:02 -0700

    Adde 'russianPostalCodeChecker.js'

---
+++ js/7kyu/russianPostalCodeChecker.js
@@ -0,0 +1,32 @@
+// https://www.codewars.com/kata/552e45cc30b0dbd01100001a/train/javascript
+
+function zipvalidate(postcode) {
+    if (postcode.length != 6 || postcode[0] == 0 || postcode[0] == 5 || postcode[0] == 7 || postcode[0] == 8 || postcode[0] == 9) {
+        return false
+    }
+    
+    for (let i = 0; i <= postcode.length - 1; i++) {
+        if (postcode[i] == '1' 
+        || postcode[i] == '2' 
+        || postcode[i] == '3'
+        || postcode[i] == '4'
+        || postcode[i] == '5'
+        || postcode[i] == '6'
+        || postcode[i] == '7'
+        || postcode[i] == '8'
+        || postcode[i] == '9'
+        || postcode[i] == '0' ) {
+        
+        }else{
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(zipvalidate( '198328' ))
+console.log(zipvalidate( '12A483' ))
+console.log(zipvalidate( '1@63' ))
+console.log(zipvalidate( '598328' ))
+
+//console.log(typeof('1@63'))
