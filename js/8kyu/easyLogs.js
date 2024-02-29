23fca8ff4d3ddca7a0ca2d9fc91de7a24d843aa8
jekajolob <jekajolob@gmai.com>
2024-02-28 22:15:30 -0800

    added "easy logs"

---
+++ js/8kyu/easyLogs.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/5b68c7029756802aa2000176/train/javascript
+
+function logs(x , a, b){
+    return (Math.log(a*b) / Math.log(x));
+}
+
+console.log(logs(10, 2, 3)); // 0.7781512503836435
+
+let a = 9;
+console.log(Math.log(a))
