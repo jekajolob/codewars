e05979c6d4fe97683e48d8fd1d1bc42cbf4744cc
jekajolob <jekajolob@gmai.com>
2024-10-01 23:55:52 -0700

    first commit

---
+++ js/8kyu/reversingWordsInAString.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/57a55c8b72292d057b000594/train/javascript
+
+function reverse(string){
+    return string.split(' ').reverse().join(' ')
+  }
+
+console.log(reverse('I am an expert at this'))
