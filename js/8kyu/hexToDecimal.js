13e0d829c79012fb501b1f82046b0390fcd69826
jekajolob <jekajolob@gmai.com>
2024-10-24 18:49:52 -0700

    first commit

---
+++ js/8kyu/hexToDecimal.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/57a4d500e298a7952100035d/train/javascript
+
+function hexToDec(hexString){
+    return parseInt(hexString, 16)
+}
+
+  console.log(hexToDec("10")) // 16
+  console.log(hexToDec("FF")) // 16
