b2ad2c4138ca007c695a3e1a7ba25b25d363137d
jekajolob <jekajolob@gmai.com>
2024-11-12 13:11:23 -0800

    first commit

---
+++ js/8kyu/contTheMonkeys.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/56f69d9f9400f508fb000ba7/train/javascript
+
+function monkeyCount(n) {
+    const arr = [];
+    for(i=1; i <= n; i++){
+        arr.push(i)
+        }
+        return arr
+
+    }
+
+    console.log(monkeyCount(5))
