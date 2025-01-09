3203fbfb7d71aee1c5316245a8e91753071bb663
jekajolob <jekajolob@gmai.com>
2025-01-09 11:33:55 -0800

    first commit

---
+++ js/8kyu/classyExtentions.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/55a14aa4817efe41c20000bc/train/javascript
+
+class Cat extends Animal {
+    speak() {
+        return `${this.name} meows.`
+    }
+  
+}
---
+++ js/8kyu/generateRangeOfIntegers.js
@@ -0,0 +1,8 @@
+function generateRange(min, max, step){
+    let arr = []
+    for (let i = min; i <= max; i += step) {
+        arr.push(i)
+    }
+    return arr
+
+}
