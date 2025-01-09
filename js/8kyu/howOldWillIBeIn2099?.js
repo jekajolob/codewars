35a2217c82fea63eed903646749d508cfb38dd9a
jekajolob <jekajolob@gmai.com>
2025-01-08 21:39:04 -0800

    first commit

---
+++ js/8kyu/howOldWillIBeIn2099?.js
@@ -0,0 +1,20 @@
+
+function  calculateAge(bornYear, currentYear) {  
+    if(bornYear == currentYear){
+      return "You were born this very year!"
+  }if(bornYear < currentYear){
+    if(currentYear - bornYear == 1){
+      return "You are 1 year old."
+  }else{
+    return `You are ${currentYear - bornYear} years old.`
+  }
+}else{
+    if(bornYear - currentYear == 1){
+        return "You will be born in 1 year."
+    }else{
+        return `You will be born in ${bornYear - currentYear} years.`
+    }
+    }
+}
+  
+  console.log(calculateAge(2000, 2090)) //You are 90 years old.
