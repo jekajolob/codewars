f0b4f4007c5e822fcca9b9e5c93eaebb49db9ee8
jekajolob <jekajolob@gmai.com>
2025-01-08 21:55:31 -0800

    Updated comments

--- js/6kyu/sumOfDigits.js
+++ js/6kyu/sumOfDigits.js
@@ -1,14 +1,16 @@
 //https://www.codewars.com/kata/541c8630095125aba6000c00/train/javascript
 
 function digitalRoot(n) {
-    let stroke = '';
+    let arr = Array.from(String(n), Number);
+    let stroke = 0;
 
-        for (i = 0; i < n.length; i++) {
-            stroke += n[i]
+        for (i = 0; i < arr.length; i++) {
+            stroke += arr[i]
         }
         return stroke;
     }
 
-console.log(digitalRoot(16))
+console.log(digitalRoot(4569999999999))
 
 //not finished
+// Ho4u Maksa v zhopu!
