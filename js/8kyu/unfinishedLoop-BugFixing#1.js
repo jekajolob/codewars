fe8470bca63d9c273bda71b7622fdd523b20e1f8
jekajolob <jekajolob@gmai.com>
2025-02-13 20:14:02 -0800

    first commit

---
+++ js/8kyu/unfinishedLoop-BugFixing#1.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/55c28f7304e3eaebef0000da/train/javascript
+
+function createArray(number) {
+    const newArray = [];
+    for (let counter = 1; counter <= number; counter++) {
+        newArray.push(counter);
+    }
+    return newArray;
+}
