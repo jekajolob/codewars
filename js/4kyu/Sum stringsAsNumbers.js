3893b53741d345880c3a4c1994bef89c1c6cbdd3
jekajolob <jekajolob@gmai.com>
2025-02-21 22:59:42 -0800

    first commit

---
+++ js/4kyu/Sum stringsAsNumbers.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/5324945e2ece5e1f32000370/train/javascript
+
+function sumStrings(a,b) { 
+    
+
+}
---
+++ js/8kyu/theWide-MouthedFrog!.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89/train/javascript
+
+function mouthSize(animal) {
+    if(animal == "alligator" || animal == "ALLIGATOR"){
+      return "small"
+    }else{
+      return "wide"
+    }
+  }
