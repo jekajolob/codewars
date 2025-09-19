ef06bdfdee7b46cffa3e1df45315949e295b8106
jekajolob <jekajolob@gmai.com>
2025-09-19 10:30:26 -0700

    added "bASIC:MakingSixToast.js"

---
+++ js/8kyu/bASIC:MakingSixToast.js
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/5834fec22fb0ba7d080000e8/train/javascript
+
+function sixToast(num) {
+  if (num >= 6){
+    return num - 6
+  }else{
+    return 6 - num
+  }
+}
+
+console.log(sixToast(6))
+console.log(sixToast(17))
+console.log(sixToast(3))
+
+
