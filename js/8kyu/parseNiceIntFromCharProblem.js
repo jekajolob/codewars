5c4e571f4a30e57b437e549018fd2c774a70d06b
jekajolob <jekajolob@gmai.com>
2025-10-03 00:03:30 -0700

    added parseNiceIntFromCharProblem.js"

---
+++ js/8kyu/parseNiceIntFromCharProblem.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1/train/javascript
+
+function getAge(inputString){
+  return parseInt(inputString, 10)
+// return the girl's correct age as an integer. Happy coding :) 
+}
+
+console.log(getAge("4 years old"))
