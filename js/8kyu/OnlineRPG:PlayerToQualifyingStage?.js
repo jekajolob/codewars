63189240615475f462c2be04b629e07ead8d036e
Yevhenii Zholob <yevzholob@gmail.com>
2026-01-09 10:32:17 -0800

    added playerRankUp function to determine player advancement

---
+++ js/8kyu/OnlineRPG:PlayerToQualifyingStage?.js
@@ -0,0 +1,20 @@
+//https://www.codewars.com/kata/55849d76acd73f6cc4000087/train/javascript
+
+function playerRankUp (points) {
+    if (points >= 100){
+        return "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up."
+    }else{
+        return false
+    }
+}
+
+console.log(playerRankUp(99))
+
+/* const {assert} = require('chai')
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(playerRankUp(64), false)
+    assert.strictEqual(playerRankUp(101), "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.")
+  });
+}); */
